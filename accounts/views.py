576e82f7a9d605e485413c471fd09aaea37381ec
hlmj3 <hlmj3017@naver.com>
2023-08-30 13:43:28 +0900

    accounts Signup 기능 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from .forms import CustomUserCreationForm
 # Create your views here.
 def signup(request):
     if request.method == 'POST':
-        pass
+        form = CustomUserCreationForm(request.POST, request.FILES) # 아이디, 패스워드 / 사진, 프로필 이미지
+        if form.is_valid():
+            form.save()
+            return redirect('posts:index')
+
 
     else:
-        pass
+        form = CustomUserCreationForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'accounts/form.html', context) # 이름 중복 충돌 제거 
+
+    # 'form.html' 안 만들었는데 동작을 하는 이유
+    # -> posts의 form.html이 먼저 만들어서 우선순위로 작동함
+
