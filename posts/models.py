bd51981048b6d81b99a2e4ee9dd7c40720bf8555
hlmj3 <hlmj3017@naver.com>
2023-08-30 11:41:46 +0900

    resized

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,15 @@
 from django.db import models
-
+from django_resized import ResizedImageField
 # Create your models here.
 
 class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    image = models.ImageField(upload_to='image/%Y/%m') # db에 저장을 해주세요 근데 사진은 저장이 안 됨 / 글자와 숫자만 가능
+    # image = models.ImageField(upload_to='image/%Y/%m') # db에 저장을 해주세요 근데 사진은 저장이 안 됨 / 글자와 숫자만 가능
+    image = ResizedImageField(
+        size=[500, 500],
+        crop=['middle', 'center'],
+        upload_to='image/%Y/%m'
+
+    )
